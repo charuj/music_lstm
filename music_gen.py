0c5e589213dccdb4760f78628ce085db3d356aef
charuj <clmjaiswal@gmail.com>
2016-04-20 07:22:31 -0400

    hacked batch training adjusted meta parameters

--- music_gen.py
+++ music_gen.py
@@ -5,26 +5,34 @@ import tensorflow as tf
 from tensorflow.models.rnn import rnn_cell
 from tensorflow.models.rnn import rnn
 
+def normalize(data):
+    mean = data.mean(axis=0)
+    std = 0.5*data.max(axis=0)
+    data = (data-mean)/std
+    data = data * 255
+    data = data + 127
+    return data
 
 
 bach_set= read('cell-8b-8khz.wav')
 bach_array= np.array(bach_set[1]) # made this int, should it be float instead
 
+bach_array=normalize(bach_array)
 # TODO: batch training instead of full dataset all at once
 # TODO: multiple layers
 # TODO: dropout
 # TODO: graph training and validation error
 # TODO: output file as wav
-
+# TODO: increase validation set size
 
 
 #Defining some hyper-params
 input_size = 1      #num_units and input_size will be the same
-num_units = input_size*2    #this is the parameter for input_size in the basic LSTM cell
+num_units = input_size*100    #this is the parameter for input_size in the basic LSTM cell
 
 
 seq_len = 80
-num_epochs=10
+num_epochs=18000
 
 def data_process(bach_array):
 
@@ -53,8 +61,7 @@ def data_process(bach_array):
     return target_list, bach_lists, num_seq
 
 
-training_set_size = (bach_array.shape[0]*3)/4
-training_targets, training_input, num_seq = data_process(bach_array[:training_set_size])
+training_set_size = (bach_array.shape[0]*3)/200
 
 #
 # batch_size = bach_array
@@ -70,11 +77,11 @@ training_targets, training_input, num_seq = data_process(bach_array[:training_se
 #     bach_lists.append(array_to_append)
 
 #training_input= bach_lists[0:(total_seq*3)/4]
-print (len(training_input))
-print (training_input[0].shape)
+# print (len(training_input))
+# print (training_input[0].shape)
 #training_targets= np.asarray(target_list[0:(total_seq*3)/4])
 
-valid_target, valid_input, num_seq= data_process(bach_array[training_set_size:])
+valid_target, valid_input, num_seq= data_process(bach_array[-training_set_size:])
 
 
 # MODEL
@@ -87,10 +94,10 @@ outputs, states = rnn.rnn(cell, inputs, dtype=tf.float32)
 
 outputs2 = outputs[-1]
 
-W_o = tf.Variable(tf.random_normal([num_units,input_size], stddev=0.01))
-b_o = tf.Variable(tf.random_normal([input_size], stddev=0.01))
+W_o = tf.Variable(tf.random_normal([num_units,input_size], stddev=0.1))
+b_o = tf.Variable(tf.random_normal([input_size], stddev=0.1))
 
-outputs3 = tf.tanh(tf.matmul(tf.tanh(outputs2),W_o) + b_o)
+outputs3 = tf.nn.relu(tf.matmul(tf.nn.relu(outputs2),W_o) + b_o)
 
 cost = tf.reduce_mean(tf.abs(outputs3-result))
 
@@ -102,11 +109,13 @@ with tf.Session() as sess:
 
     tf.initialize_all_variables().run()     #initialize all variables in the model
     #temp_dict= {inputs:training_input,result:training_targets}
-    temp_dict = {inputs[i]:training_input[i] for i in range(seq_len-1)}
-    temp_dict.update({result: training_targets})
+
     val_dict = {inputs[i]:valid_input[i] for i in range(seq_len-1)}
     val_dict.update({ result: valid_target})
     for k in range(num_epochs):
+        training_targets, training_input, num_seq = data_process(bach_array[(k*training_set_size)%(bach_array.shape[0]-training_set_size):((k+1)*training_set_size)%(bach_array.shape[0]-training_set_size)])
+        temp_dict = {inputs[i]:training_input[i] for i in range(seq_len-1)}
+        temp_dict.update({result: training_targets})
         sess.run(train_op,feed_dict=temp_dict)
         train_cost = sess.run(cost, feed_dict = temp_dict )
         valid_cost= sess.run(cost, feed_dict = val_dict)
